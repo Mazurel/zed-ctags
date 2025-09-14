97418a4b108b933f74f5adbaa27a9808565728c4
Mateusz Mazur <mateusz.mazur@e.email>
2025-09-14 15:05:13 +0200

    Refactor url code a little
    Signed-off-by: Mateusz Mazur <mateusz.mazur@e.email>

--- src/ctags_lsp.rs
+++ src/ctags_lsp.rs
@@ -6,17 +6,19 @@ const CTAGS_LSP_FOLDER_NAME: &str = "ctags-lsp-project";
 const CTAGS_LSP_BINARY_NAME: &str = "ctags-lsp";
 
 fn get_ctags_lsp_binary_url() -> Result<String, ErrorMessage> {
-    let url = match zed::current_platform() {
-        (zed::Os::Linux, zed::Architecture::X8664) => "https://github.com/netmute/ctags-lsp/releases/download/v0.8.1/ctags-lsp_Linux_x86_64.tar.gz",
-        (zed::Os::Linux, zed::Architecture::Aarch64) => "https://github.com/netmute/ctags-lsp/releases/download/v0.8.1/ctags-lsp_Linux_arm64.tar.gz",
-        (zed::Os::Mac, zed::Architecture::X8664) => "https://github.com/netmute/ctags-lsp/releases/download/v0.8.1/ctags-lsp_Darwin_x86_64.tar.gz",
-        (zed::Os::Mac, zed::Architecture::Aarch64) => "https://github.com/netmute/ctags-lsp/releases/download/v0.8.1/ctags-lsp_Darwin_arm64.tar.gz",
-        (zed::Os::Windows, zed::Architecture::X8664) => "https://github.com/netmute/ctags-lsp/releases/download/v0.8.1/ctags-lsp_Windows_x86_64.tar.gz",
-        (zed::Os::Windows, zed::Architecture::Aarch64) => "https://github.com/netmute/ctags-lsp/releases/download/v0.8.1/ctags-lsp_Windows_arm64.tar.gz",
-        (os, arch) => {
-            Err(format!("Unsupported platform: {:?} {:?}", os, arch).to_string())?
-        }
+    let tarball = match zed::current_platform() {
+        (zed::Os::Linux, zed::Architecture::X8664) => "ctags-lsp_Linux_x86_64.tar.gz",
+        (zed::Os::Linux, zed::Architecture::Aarch64) => "ctags-lsp_Linux_arm64.tar.gz",
+        (zed::Os::Mac, zed::Architecture::X8664) => "ctags-lsp_Darwin_x86_64.tar.gz",
+        (zed::Os::Mac, zed::Architecture::Aarch64) => "ctags-lsp_Darwin_arm64.tar.gz",
+        (zed::Os::Windows, zed::Architecture::X8664) => "ctags-lsp_Windows_x86_64.tar.gz",
+        (zed::Os::Windows, zed::Architecture::Aarch64) => "ctags-lsp_Windows_arm64.tar.gz",
+        (os, arch) => Err(format!("Unsupported platform: {:?} {:?}", os, arch).to_string())?,
     };
+    let url = format!(
+        "https://github.com/netmute/ctags-lsp/releases/download/v0.8.1/{}",
+        tarball
+    );
     println!("Downloading ctags lsp from {}", url);
 
     Ok(url.to_string())
